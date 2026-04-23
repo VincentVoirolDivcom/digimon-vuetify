8e5a1910804c3fa2cfe1d15d63478dcaec647ae1
Vincent Voirol <vincent.voirol@divcom.ch>
2026-04-23 11:42:16 +0200

    digimonStore.js added with axios

--- src/stores/digimonStore.js
+++ src/stores/digimonStore.js
@@ -1,13 +1,12 @@
 import { defineStore } from 'pinia'
+import api from "@/plugins/axios";
 
-const BASE_URL = 'https://digi-api.com/api/v1'
 const PAGE_SIZE = 20
 
 export const useDigimonStore = defineStore('digimon', {
   state: () => ({
     digimons: [],
     levels: [],
-    currentPage: 0,
     totalPages: 1,
     isLoading: false,
     error: null,
@@ -15,23 +14,17 @@ export const useDigimonStore = defineStore('digimon', {
   getters: {},
   actions: {
     /**
-     * Charge les Digimons par batch de 20 pour la pagination
+     * Charge les 20 premiers Digimons
      * @returns {Promise<void>}
      */
     async fetchDigimons() {
       this.isLoading = true
+
       try {
-        const apiPage = this.currentPage
-        const response = await fetch(`${BASE_URL}/digimon?page=${apiPage}&pageSize=${PAGE_SIZE}`)
+        const response = await api.get(`/digimon?pageSize=${PAGE_SIZE}`)
+        const datas = await response.data
 
-        if (!response.ok) {
-          throw new Error(`Erreur HTTP : ${response.status}`)
-        }
-
-        const datas = await response.json()
-
-        this.digimons = datas
-        this.totalPages = datas.pageable.totalPages
+        this.digimons = datas.content
       } catch (error) {
         console.error('Erreur lors du chargement des Digimons :', error)
         this.error = 'Impossible de charger les Digimons'
@@ -47,13 +40,8 @@ export const useDigimonStore = defineStore('digimon', {
     async fetchLevels() {
       this.isLoading = true
       try {
-        const response = await fetch(`${BASE_URL}/level`)
-
-        if (!response.ok) {
-          throw new Error(`Erreur HTTP : ${response.status}`)
-        }
-
-        const datas = await response.json()
+        const response = await api.get(`/level`)
+        const datas = await response.data
         const totalElements = datas.pageable.totalElements
         const elementsOnCurrentPage = datas.pageable.elementsOnPage
         // Calculer le nombre d'itérations pour avoir la totalité des données des niveaux des Digimons
@@ -62,8 +50,8 @@ export const useDigimonStore = defineStore('digimon', {
         let allLevels = datas.content.fields
 
         for (let page = 1; page < nbPages; page++) {
-          const nextRes  = await fetch(`${BASE_URL}/level?page=${page}`)
-          const nextData = await nextRes.json()
+          const nextRes  = await api.get(`/level?page=${page}`)
+          const nextData = await nextRes.data
           allLevels = allLevels.concat(nextData.content.fields)
         }
 
